ce4c109176c5edef2ea9c77705258fdbe779ca3c
Jonathan <78378288+Jonathan25J@users.noreply.github.com>
2021-02-22 18:47:26 +0100

    Update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const client = new discord.Client();
 
 // command handler
 client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
 
 client.login(process.env.token);
 
@@ -20,62 +21,72 @@ fs.readdir("./commands", (err, files) => {
 
   var jsFiles = files.filter(f => f.split(".").pop() === "js");
 
-  if (jsFiles.length <= 0){
-      console.log("Couldn't find files");
-      return;
+  if (jsFiles.length <= 0) {
+    console.log("Couldn't find files");
+    return;
   }
 
-   jsFiles.forEach((f, i) => {
+  jsFiles.forEach((f, i) => {
 
-   var fileGet = require(`./commands/${f}`);
-    
-   client.commands.set(fileGet.help.name, fileGet);
-   })
+    var fileGet = require(`./commands/${f}`);
+
+    client.commands.set(fileGet.help.name, fileGet);
+
+    if (fileGet.help.aliases) {
+
+      fileGet.help.aliases.forEach(w => {
+
+        client.aliases.set(w, fileGet.help.name)
+
+      })
+    }
+
+  })
 
 });
 
-client.on("ready", async () =>{
+client.on("ready", async () => {
 
   console.log(`The bot ${client.user.username} is online.`);
   client.user.setActivity(".help");
 });
 
-client.on("message", async message =>{
-    if(message.author.bot) return;
+client.on("message", async message => {
+  if (message.author.bot) return;
 
-    if(message.channel.type == "dm") return;
+  if (message.channel.type == "dm") return;
 
-    var prefix = config.prefix;
+  var prefix = config.prefix;
 
-    var messageAray = message.content.split(" ");
+  var messageAray = message.content.split(" ");
 
-    var command = messageAray[0];
+  var command = messageAray[0];
 
-    // command handler
-    var arguments = messageAray.slice(1);
+  // command handler
+  var arguments = messageAray.slice(1);
 
-    var commands = client.commands.get(command);
+  var commands = client.commands.get(command) || client.commands.get(client.aliases.get(command));
 
-    var ops = {
-      active: active
-    }
+  var ops = {
+    active: active
+  }
 
-    if (commands) commands.run(client, message, arguments, ops);
-    
+  if (commands) commands.run(client, message, arguments, ops);
+
 });
 
 client.on("guildMemberAdd", member => {
 
-    const channelC = JSON.parse(fs.readFileSync("./database/channels.json", "utf8"));
-    const guildID = member.guild.id;
+  const channelC = JSON.parse(fs.readFileSync("./database/channels.json", "utf8"));
+  const guildID = member.guild.id;
 
-    if (!channelC[guildID].welcome) return;
-    var message = channelC[guildID].welcome.message;
+  if (!channelC[guildID].welcome) return;
+  var message = channelC[guildID].welcome.message;
 
-    if (!message) return;
-    var cSelected = channelC[guildID].welcome.channel;             
-    var channel = member.guild.channels.cache.find(channel => channel.name === cSelected);
-    if (!channel) return;
+  if (!message) return;
+  var cSelected = channelC[guildID].welcome.channel;
+  var channel = member.guild.channels.cache.find(channel => channel.name === cSelected);
+  if (!channel) return;
 
   channel.send(`${member} ${message}`);
 
